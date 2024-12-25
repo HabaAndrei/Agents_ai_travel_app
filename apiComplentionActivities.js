03abf7042d5430a6073cc2542d221ab063f9f042
HabaAndrei <habaconstantin45@gmail.com>
2024-12-25 19:59:09 +0200

    add zodResponseFormat for apiCompletionActivities

--- apiComplentionActivities.js
+++ apiComplentionActivities.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const  OpenAI = require('openai');
 const {API_KEY_OPENAI} = process.env;
 const openai = new OpenAI({ apiKey: API_KEY_OPENAI });
+const { zodResponseFormat } = require("openai/helpers/zod");
+const  z = require("zod");
 
 class ApiComplentionActivities {
 
@@ -11,8 +13,8 @@ class ApiComplentionActivities {
     this.country = country;
   }
 
-  // universal function to call open ai with json response only
-  async LlmCallWithJsonResponse(systemPrompt, userPrompt){
+  // universal function to call open ai with zod response only
+  async LlmCallWithZodResponseFormat(systemPrompt, userPrompt, Response){
     try {
       const completion = await openai.chat.completions.create({
         messages: [{
@@ -23,13 +25,13 @@ class ApiComplentionActivities {
           'content': userPrompt
         }],
         model: 'gpt-4o-mini',
-        response_format: { "type": "json_object" },
+        response_format: zodResponseFormat(Response, "response"),
         temperature: 0,
       });
 
       let result = completion.choices[0]?.message?.content;
       if(typeof result === 'string')result = JSON.parse(result);
-      return {isResolved: true, data: result};
+      return {isResolved: true, data: result?.response};
     }catch(err){
       console.log({err});
       return {isResolved: false, err};
@@ -46,17 +48,17 @@ class ApiComplentionActivities {
           Return true if the location has both local and tourist attractions.
           Return false if it offers only one category (either local or tourist spots).
         \n 2. On a scale of 1 to 3, assess how many places are available to visit in that location.
-        \n Response: The response shoul be in this format \n
-        {
-          data: {
-            local_places_and_tourist_places: true,
-            scale_visit: 3
-          }
-        }
       `;
-
       const textPromptUser = 'Location: ' + this.city + '  from  ' + this.country;
-      const resultParamsAboutLocationLlm = await this.LlmCallWithJsonResponse(textPromptSystem, textPromptUser);
+      const JsonSchema = z.object({
+        response: z.object({
+          data: z.object({
+            local_places_and_tourist_places: z.boolean(),
+            scale_visit: z.number()
+          })
+        })
+      })
+      const resultParamsAboutLocationLlm = await this.LlmCallWithZodResponseFormat(textPromptSystem, textPromptUser, JsonSchema);
 
       // If the request to OpenAI fails, I will call the function again
       if(!resultParamsAboutLocationLlm.isResolved){
@@ -72,14 +74,17 @@ class ApiComplentionActivities {
     try{
       const textPromptSystem = `
         \n Task: You receive a location as input and return a JSON with various activities available in that location, specific in that location.
-        \n Technique: If the task is complex, split it into subtasks. Always run a math check to ensure accurate results.
         \n Note: Answer as general as possible, not specic, let it be like a category.
-        \n Response: The answer should be sent only in json form. eg. {'activities' : {'1': 'sport', '2': 'history', '3': 'wellness', etc...} }
       `;
       const textPromptUser = 'Location: ' + this.city + '  from  ' + this.country;
+      const JsonSchema = z.object({
+        response: z.object({
+          activities: z.array(z.string()).describe('Here should be the activities I can do in that location, e.g., [Sport, History, Wellness, etc.]')
+        })
+      });
 
       // create request to open ai to recive activities
-      const resultCreateActivitiesLlm = await this.LlmCallWithJsonResponse(textPromptSystem, textPromptUser);
+      const resultCreateActivitiesLlm = await this.LlmCallWithZodResponseFormat(textPromptSystem, textPromptUser, JsonSchema);
       if(!resultCreateActivitiesLlm.isResolved){
         return this.createActivities();
       }
