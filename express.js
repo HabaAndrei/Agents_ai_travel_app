c38c25d137cab136887048625b441ff2346fcd43
Haba Andrei <habaconstantin45@gmail.com>
2025-04-18 13:04:49 +0300

    test fails

--- express.js
+++ express.js
@@ -13,8 +13,8 @@ const validateFieldsAiGeneration = require('./middlewares/validateFieldsAiGenera
 const authenticatedUser = require('./middlewares/authenticatedUser.js');
 
 app.use(cors());
-app.use(express.json({ limit: '5000mb' }));
-app.use(express.urlencoded({ limit: '5000mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
