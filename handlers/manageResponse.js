536513aa022ff5f495a044722bbe5826a5d1d145
Haba Andrei <habaconstantin45@gmail.com>
2025-04-27 22:15:54 +0300

    only two indentations

--- handlers/manageResponse.js
+++ handlers/manageResponse.js
@@ -2,11 +2,11 @@ const Firebase = require('../providers/Firebase.js');
 const db = new Firebase().db;
 
 module.exports = async (functionName, response) => {
-    if (response.isResolved) return;
+  if (response.isResolved) returș;
 
-    await db.collection('server_errors').add({
-        functionName, response, time: new Date()
-    });
+  await db.collection('server_errors').add({
+    functionName, response, time: new Date()
+  });
 
-    return;
+  return;
 }
