4a624b338ff2f874ec7b37defb1261d8b4fb9018
Haba Andrei <habaconstantin45@gmail.com>
2025-05-04 09:50:04 +0300

    update tests for activities

--- tests.test.js
+++ tests.test.js
@@ -4,6 +4,7 @@ const countriesCities = require('./tests/countriesCities.json');
 const activities = require('./tests/activities.json');
 const locations = require('./tests/locations.json');
 const EnvConfig = require('./providers/EnvConfig.js');
+const crypto = require("crypto")
 
 const serverAddress = EnvConfig.getInstance().get('URL_SERVER')
 
@@ -11,27 +12,41 @@ const serverAddress = EnvConfig.getInstance().get('URL_SERVER')
 // command: jest tests.test.js
 
 
+function createHeader(body){
+  const customToken = EnvConfig.getInstance().get('AUTHORIZATION_CUSTOM_TOKEN');
+  let token = crypto.createHash('sha256').update(JSON.stringify(body) + customToken).digest('hex')
+  return {
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
 ////////////////////////////////////////////////////
 // test for activities
 
 async function generateActivities({city, country}){
-  const result = await axios.post(serverAddress, {
+  const body = {
     generationType: 'generateActivities', city, country
-  });
+  }
+  const result = await axios.post(serverAddress, body, createHeader(body));
   return result.data
 }
 
-test.each(countriesCities)('verify response from activities generator', async ({city, country}) => {
-  const result = await generateActivities({city, country});
+function testActivities(){
+  test.each(countriesCities)('verify response from activities generator', async ({city, country}) => {
+    const result = await generateActivities({city, country});
 
-  expect(result.isResolved).toBe(true);
-  expect(result.data.activities[0]).toBeDefined();
-  expect(result.paramsLocation.isResolved).toBe(true);
-  expect(result.paramsLocation.data).toBeDefined();
-  expect(result.paramsLocation.data.local_places_and_tourist_places).toBe(true || false);
-  expect(result.paramsLocation.data.scale_visit).toBeDefined();
-}, 15000)
-
+    expect(result.isResolved).toBe(true);
+    expect(result.data.activities[0]).toBeDefined();
+    expect(result.paramsLocation.isResolved).toBe(true);
+    expect(result.paramsLocation.data).toBeDefined();
+    expect(result.paramsLocation.data.local_places_and_tourist_places).toBe(true || false);
+    expect(result.paramsLocation.data.scale_visit).toBeDefined();
+  }, 15000)
+}
+testActivities()
 
 
 
@@ -48,31 +63,33 @@ async function generateLocations({city, country, customActivity, selectedActivit
   return result.data;
 }
 
-test.each(activities)('verify response from locations generator', async (details) => {
+function testLocations(){
+  test.each(activities)('verify response from locations generator', async (details) => {
 
-  const result = await generateLocations(details);
+    const result = await generateLocations(details);
 
-  expect(result.isResolved).toBe(true);
-  expect(result.data).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({
-        name: expect.any(String),
-        address: expect.any(String),
-        place_id: expect.any(String),
-        urlLocation: expect.any(String),
-        website: expect.any(String),
-        geometry_location: expect.any(Object),
-        arrayProgramPlace: expect.any(Array),
-        arrayWithLinkImages: expect.any(Array),
-        dataTimeLocation: expect.any(Object)
+    expect(result.isResolved).toBe(true);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.any(String),
+          address: expect.any(String),
+          place_id: expect.any(String),
+          urlLocation: expect.any(String),
+          website: expect.any(String),
+          geometry_location: expect.any(Object),
+          arrayProgramPlace: expect.any(Array),
+          arrayWithLinkImages: expect.any(Array),
+          dataTimeLocation: expect.any(Object)
 
-      })
-    ]),
-    expect(result.urlImageCity).toBeDefined()
-  );
+        })
+      ]),
+      expect(result.urlImageCity).toBeDefined()
+    );
 
-}, 2000000)
-
+  }, 2000000)
+}
+// testLocations();
 
 
 // ////////////////////////////////////////////////////
@@ -93,35 +110,37 @@ async function generateProgram(){
 
 }
 
-test.each(
-  Array(20).fill()
-)('verify response from program generator', async () => {
-  const result = await generateProgram();
+function testProgram(){
+  test.each(
+    Array(20).fill()
+  )('verify response from program generator', async () => {
+    const result = await generateProgram();
 
-  expect(result.isResolved).toBe(true);
-  expect(result.data).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({
-        day: expect.any(Number),
-        title: expect.any(String),
-        date: expect.any(String),
-        activities: expect.arrayContaining([
-          expect.objectContaining({
-            time: expect.any(String),
-            place: expect.any(String),
-            address: expect.any(String),
-            urlLocation: expect.any(String),
-            place_id: expect.any(String),
-            website: expect.any(String),
-            description: expect.any(String),
-            info: expect.any(String),
-            geometry_location: expect.any(Object),
-            arrayWithLinkImages: expect.any(Array),
-            dataTimeLocation: expect.any(Object),
-          })
-        ])
-      })
-    ])
-  )
-}, 15000);
-
+    expect(result.isResolved).toBe(true);
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          day: expect.any(Number),
+          title: expect.any(String),
+          date: expect.any(String),
+          activities: expect.arrayContaining([
+            expect.objectContaining({
+              time: expect.any(String),
+              place: expect.any(String),
+              address: expect.any(String),
+              urlLocation: expect.any(String),
+              place_id: expect.any(String),
+              website: expect.any(String),
+              description: expect.any(String),
+              info: expect.any(String),
+              geometry_location: expect.any(Object),
+              arrayWithLinkImages: expect.any(Array),
+              dataTimeLocation: expect.any(Object),
+            })
+          ])
+        })
+      ])
+    )
+  }, 15000);
+}
+// testProgram()
